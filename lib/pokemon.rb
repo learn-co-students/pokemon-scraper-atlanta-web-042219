bed14e5f46407284ca8860817a74ea2b37719a17
github username <dekadekadeka@gmail.com>
2019-04-30 16:43:00 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,18 +20,12 @@ class Pokemon
 
     def self.find(id, db)
         query1 = db.execute("SELECT * FROM pokemon WHERE id = #{id}").flatten
-        options = {id: query1[0], name: query1[1], type: query1[2]}
+        options = {id: query1[0], name: query1[1], type: query1[2], hp: query1[3]}
         Pokemon.new(options)
     end
 
     def alter_hp(new_hp, db)
-        # query = "UPDATE pokemon SET hp = #{new_hp} WHERE id = #{self.id}"
-        # query = <<-SQL
-        # UPDATE pokemon SET hp = ? WHERE id = ?
-        # SQL
-
         db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-        # binding.pry
     end
 
 end
